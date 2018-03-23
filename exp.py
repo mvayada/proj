51abbf3dfc28dd09bc64d80772a3a95d4d0111dd
mvayada <mvayada@provino-tech.com>
2018-03-23 19:38:56 +0530

    one more attempts

--- exp.py
+++ exp.py
@@ -3,7 +3,7 @@ import sys
 def print_details():
     print('The calculations')
     number = 0
-    for x in range(10):
+    xfor x in range(10):
         number = number +1
     print('the changed more number is : {}'.format(number))
 	#print('over')
