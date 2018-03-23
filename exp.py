89ad8eee8e6877e0d0d3e815b7630ee518ff5f3f
mvayada <mvayada@provino-tech.com>
2018-03-23 19:26:53 +0530

    checking scm polling

--- exp.py
+++ exp.py
@@ -5,7 +5,7 @@ def print_details():
     number = 0
     for x in range(10):
         number = number +1
-    print('number is : {}'.format(number))
+    print('the changed number is : {}'.format(number))
 	#print('over')
 
 if __name__ =='__main__':
