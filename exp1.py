0d23632de5f18e30c1d82a6df568f2276cb430d4
Mohammed Vayada <mvayada@provino-tech.com>
2018-03-26 12:19:23 +0530

    added hashbang line in code

---
+++ exp1.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python
+import sys
+
+def print_details():
+    print('The calculations')
+    number = 0
+    for x in range(20):
+        number = number +1
+    print('number is : {}'.format(number))
+
+if __name__ =='__main__':
+    print_details()
+    sys.exit
+
